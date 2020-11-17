5f90f8fea167caf169cc01fa005cfe39f07e3b6f
Muhammad Awais <ivylabash@gmail.com>
2020-11-17 12:29:15 +0500

    Redux setUp

---
+++ Call4CareDoc/App/helper/Color.js
@@ -0,0 +1,79 @@
+// @flow
+
+export const BLACK = {
+  light: 'rgba(0, 0, 0, 0.9)',
+  app: 'rgba(13, 12, 13, 1)',
+  dark: 'rgba(0, 0, 0, 1)',
+  darkLight: 'rgba(41, 43, 47, 0.95)',
+  placeholder: 'rgba(0, 0, 0, 0.15)',
+  divider: 'rgba(0, 0, 0, 0.05)',
+  transParent: 'rgba(52, 52, 52, 0.8)',
+  textColor: 'rgba(137, 138, 143, 1)',
+};
+
+export const WHITE = {
+  off: 'rgba(228, 228, 228, 1)',
+  dark: '#fff',
+  silver: 'rgba(192, 192, 192, 1)',
+  cream: 'rgba(245, 245, 245, 1)',
+  borderLight: 'rgba(151, 151, 151, 0.26)',
+  transParent: 'rgba(151, 151, 151, 0.8)',
+  light: 'rgba(191, 191, 191, 0.6)',
+  app: 'rgba(246, 246, 246, 1)',
+};
+
+export const BLUE = {
+  app: 'rgba(39, 51, 149, 1)',
+  off: 'rgba(177, 255, 255, 1)',
+  textInput: 'rgba(75, 87, 167, 1)',
+  facebook: 'rgba(58,87,154, 1)',
+  google: 'rgba(66, 133, 244, 1)',
+  text: 'rgba(106, 163, 255, 1)',
+  textUnderline: 'rgba(136, 181, 255, 1)',
+};
+
+export const ORANGE = {
+  defult: 'rgba(204, 154, 74, 1)',
+};
+
+export const INDICATORBLUE = {
+  default: 'rgba(119, 191, 255, 1)',
+};
+
+export const APPCOLOR = {
+  default: 'rgba(251, 117, 123, 1)',
+  button: 'rgba(239, 70, 117, 1)',
+};
+
+export const GRAY = {
+  light: '#f1f1f2',
+  dark: '#8e8e93',
+  separator: 'rgba(77, 77, 77, 0.78)',
+};
+
+export const LIGHTBLUE = {
+  default: '#77BFFF',
+};
+
+export const GREEN = {
+  deafult: 'rgba(10,140,85, 1)',
+  app: 'rgba(96, 174, 116, 1)',
+  border: 'rgba(96, 174, 116, 0.7)',
+  light: 'rgba(10,140,85, 0.4)',
+};
+
+export const PURPLE = {
+  default: 'rgba(104, 3, 153, 1)',
+  light: 'rgba(104, 3, 153,0.4)',
+  app: 'rgba(94, 1, 140, 1)',
+  mediumHeigh: 'rgba(104, 19, 120,1)',
+  medium: 'rgba(104, 19, 152,0.6)',
+};
+
+export const RED = {
+  deafult: 'rgba(221,10,50, 1)',
+  light: 'rgba(255,0,0, 0.4)',
+  app: 'rgba(250,77,103, 1)',
+};
+
+export const CLEAR = 'transparent';
---
+++ Call4CareDoc/App/navigation/AppNavigator.js
@@ -0,0 +1,35 @@
+import {Animated, Easing} from 'react-native';
+import {createSwitchNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import Splash from '../screen/Splash';
+
+// https://github.com/react-community/react-navigation/issues/1254
+const noTransitionConfig = () => ({
+  transitionSpec: {
+    duration: 0,
+    timing: Animated.timing,
+    easing: Easing.linear,
+  },
+});
+
+// login stack
+const LoginStack = createStackNavigator({
+  Splash: {
+    screen: Splash,
+    navigationOptions: {headerShown: false, gestureEnabled: false},
+  },
+});
+
+// eslint-disable-next-line no-undef
+export default SwitchNavigator = createSwitchNavigator(
+  {
+    AuthLoading: LoginStack,
+    App: LoginStack,
+    Auth: LoginStack,
+  },
+  {
+    initialRouteName: 'AuthLoading',
+    transitionConfig: noTransitionConfig,
+  },
+);
